21d6eb509b039e812b3857cec1b8909120e7b702
Ivan Gankevich <igankevich@ya.ru>
2017-04-01 17:28:51 +0300

    Add -h option to dcmt. Add progress bar.

--- src/dcmt.cc
+++ src/dcmt.cc
@@ -17,20 +17,46 @@ generate_mersenne_twisters(std::string filename, size_t num_generators) {
 	arma::parallel_mt_seq<> seq(seed);
 	std::ofstream out(filename);
 	std::ostream_iterator<arma::mt_config> out_it(out);
-	std::generate_n(out_it, num_generators, std::ref(seq));
+	size_t i = 0;
+	std::generate_n(
+		out_it,
+		num_generators,
+		[&i,&seq,num_generators] () {
+			++i;
+			std::clog << "Finished "
+				<< '[' << i << '/' << num_generators << ']'
+				<< std::endl;
+			return seq();
+		}
+	);
+}
+
+void
+usage(char* argv0) {
+	std::cout
+		<< "USAGE: "
+		<< (argv0 == nullptr ? "arma-dcmt" : argv0)
+		<< " -n NUM_GENERATORS -o OUTFILE -h\n";
 }
 
 int
 main(int argc, char* argv[]) {
 	size_t ngenerators = 128;
 	std::string filename = arma::config::mt_config_file;
+	bool help_requested = false;
 	int opt = 0;
-	while ((opt = ::getopt(argc, argv, "n:o:")) != -1) {
+	while ((opt = ::getopt(argc, argv, "n:o:h")) != -1) {
 		if (opt == 'n') {
 			ngenerators = std::atoi(::optarg);
 		} else if (opt == 'o') {
 			filename = ::optarg;
+		} else if (opt == 'h') {
+			help_requested = true;
 		}
+	}
+	if (help_requested) {
+		usage(argv[0]);
+		return 0;
 	}
 	if (optind != argc) {
 		std::clog << "Bad file argument." << std::endl;
