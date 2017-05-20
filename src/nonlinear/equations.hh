de897337c070f261a84bdedb4da26c1164a35638
Ivan Gankevich <igankevich@ya.ru>
2017-05-20 15:40:41 +0300

    Add CDF transformation function and equations.

---
+++ src/nonlinear/equations.hh
@@ -0,0 +1,82 @@
+#ifndef NONLINEAR_EQUATIONS_HH
+#define NONLINEAR_EQUATIONS_HH
+
+#include <blitz/array.h>
+
+namespace arma {
+
+	namespace nonlinear {
+
+		/**
+		\brief Defines equality between cumulative distribution functions
+		       at a specified point.
+		\date 2017-05-20
+		\author Ivan Gankevich
+		*/
+		template<class T, class CDF>
+		class Equation_CDF {
+
+			/// CDF (LHS of the equation).
+			CDF _cdf;
+
+			/// Value of CDF at a specified point (RHS of the equation).
+			T _cdfy;
+
+		public:
+			inline
+			Equation_CDF(CDF func, T y) noexcept:
+			_cdf(func),
+			_cdfy(y)
+			{}
+
+			inline T
+			operator()(T x) {
+				return _cdf.cdf(x) - _cdfy;
+			}
+
+		};
+
+		/**
+		\brief Defines equality between ACF and its Gram---Charlier
+		       series expansion at a specified point.
+		\date 2017-05-20
+		\author Ivan Gankevich
+		*/
+		template<class T>
+		class Equation_ACF {
+
+			typedef blitz::Array<T,1>& array_type;
+
+			/// Coefficients of the expansion.
+			const array_type& _coef;
+
+			/// Value of ACF at a specified point.
+			T _acfy;
+
+		public:
+			inline
+			Equation_ACF(const array_type& coefs, T y):
+			_coef(coefs),
+			_acfy(y)
+			{}
+
+			inline T
+			operator()(T x) const {
+				T sum = 0;
+				T f = 1;
+				T x2 = 1;
+				const int n = _coef.size();
+				for (int i=0; i<n; ++i) {
+					sum += _coef[i]*_coef[i]*x2/f;
+					f *= (i+1);
+					x2 *= x;
+				}
+				return sum - _acfy;
+			}
+		};
+
+	}
+
+}
+
+#endif // NONLINEAR_EQUATIONS_HH
---
+++ src/nonlinear/transforms.hh
@@ -0,0 +1,54 @@
+#ifndef NONLINEAR_TRANSFORMS_HH
+#define NONLINEAR_TRANSFORMS_HH
+
+#include <utility>
+#include "equations.hh"
+#include "linalg.hh"
+
+namespace arma {
+
+	namespace nonlinear {
+
+		/**
+		\brief Transforms cumulative distribution function from old_dist to
+		new_dist at specified grid points.
+		\date 2017-05-20
+		\author Ivan Gankevich
+
+		\param[in] grid defines points in which transformation is done
+		\param[in] old_dist current cumulative distribution function
+		\param[in] new_dist desired cumulative distribution function
+		\param[in] solver equation solver
+
+		\return a pair where the first element contains grid points in which
+		distribution was transformed, and the second element contains new
+		distribution function values.
+
+		\see arma::stats::Gaussian
+		\see linalg::Bisection
+		*/
+		template <class T, class Dist1, class Dist2, class Solver>
+		std::pair<blitz::Array<T,1>,blitz::Array<T,1>>
+		transform_CDF(
+			const Grid<T, 1>& grid,
+			Dist1 old_dist,
+			Dist2 new_dist,
+			Solver solver,
+		) {
+			const int n = grid.num_points();
+			blitz::Array<T,1> x(n), y(n);
+			for (int i=0; i<n; ++i) {
+				const int xi = grid(i);
+				x(i) = xi;
+				y(i) = solver(
+					Equation_CDF<T, Dist2>(new_dist, old_dist.cdf(xi))
+				);
+			}
+			return std::make_pair(x, y);
+		}
+	}
+
+}
+
+#endif // NONLINEAR_TRANSFORMS_HH
+
