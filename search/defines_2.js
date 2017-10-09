18732caaaaf5c001503a68f297f7613fe6b3b267
Travis CI <travis@travis-ci.org>
2017-10-09 13:18:45 +0000

    Deploy to gh-pages.

--- search/defines_2.js
+++ search/defines_2.js
@@ -2,6 +2,8 @@ var searchData=
 [
   ['check',['CHECK',['../fourier__opencl_8cc.html#a763ffd798508592c5092ac77daa5a659',1,'CHECK():&#160;fourier_opencl.cc'],['../high__amplitude__realtime__solver_8cc.html#a763ffd798508592c5092ac77daa5a659',1,'CHECK():&#160;high_amplitude_realtime_solver.cc']]],
   ['cl_5fuse_5fdeprecated_5fopencl_5f1_5f2_5fapis',['CL_USE_DEPRECATED_OPENCL_1_2_APIS',['../cl_8hh.html#a16605039b395344c0b68b435e197b8bd',1,'cl.hh']]],
+  ['cnt_5fbsc_5fcopy',['CNT_BSC_COPY',['../profile__counters_8hh.html#adc0388465b93d4d65d952d52d76d6249',1,'profile_counters.hh']]],
+  ['cnt_5fbsc_5fmarshalling',['CNT_BSC_MARSHALLING',['../profile__counters_8hh.html#a0514bca534c6075449a813034a0745f3',1,'profile_counters.hh']]],
   ['cnt_5fcopy_5fto_5fhost',['CNT_COPY_TO_HOST',['../profile__counters_8hh.html#a2d141e7470490fb57a34b02a935be9f6',1,'profile_counters.hh']]],
   ['cnt_5fharts_5fcopy_5fto_5fhost',['CNT_HARTS_COPY_TO_HOST',['../profile__counters_8hh.html#a70d575b56b49139882881dc925f037b8',1,'profile_counters.hh']]],
   ['cnt_5fharts_5ffft',['CNT_HARTS_FFT',['../profile__counters_8hh.html#a6b7c81f4bc325e463cfa4766db8721d9',1,'profile_counters.hh']]],
