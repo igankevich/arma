d4baac3b665eb5050d8acbc8d808dcb9a1b8d734
Travis CI <travis@travis-ci.org>
2018-01-23 15:45:01 +0000

    Deploy to gh-pages.

--- search/all_18.js
+++ search/all_18.js
@@ -9,6 +9,6 @@ var searchData=
   ['yoff2',['yoff2',['../structstbtt__packedchar.html#a2ec5bbd1010c9a9b7cbdeb7503dcaffa',1,'stbtt_packedchar']]],
   ['yule_5fwalker_2ecc',['yule_walker.cc',['../yule__walker_8cc.html',1,'']]],
   ['yule_5fwalker_2ehh',['yule_walker.hh',['../yule__walker_8hh.html',1,'']]],
-  ['yule_5fwalker_5fsolver',['Yule_walker_solver',['../classarma_1_1Yule__walker__solver.html#ad7506c468a1098727128b874722057ac',1,'arma::Yule_walker_solver::Yule_walker_solver(array_type acf, const T variance)'],['../classarma_1_1Yule__walker__solver.html#a8bafe374606f0ba075e6c82392c475d4',1,'arma::Yule_walker_solver::Yule_walker_solver(const Yule_walker_solver &amp;)=delete']]],
+  ['yule_5fwalker_5fsolver',['Yule_walker_solver',['../classarma_1_1Yule__walker__solver.html#ad7506c468a1098727128b874722057ac',1,'arma::Yule_walker_solver::Yule_walker_solver(array_type acf, const T variance)'],['../classarma_1_1Yule__walker__solver.html#a68405ad580246f0507561ec1315ab71f',1,'arma::Yule_walker_solver::Yule_walker_solver(array_type acf)'],['../classarma_1_1Yule__walker__solver.html#a8bafe374606f0ba075e6c82392c475d4',1,'arma::Yule_walker_solver::Yule_walker_solver(const Yule_walker_solver &amp;)=delete']]],
   ['yule_5fwalker_5fsolver',['Yule_walker_solver',['../classarma_1_1Yule__walker__solver.html',1,'arma']]]
 ];
