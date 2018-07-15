1f3a35058c1db1eb2d3e516b46b54dd96199ac21
Ivan Petryakov <franceskoizump@gmail.com>
2018-07-15 20:12:43 +0300

    implementation of derivative on OpenCL

--- src/velocity/high_amplitude_realtime_solver.cc
+++ src/velocity/high_amplitude_realtime_solver.cc
@@ -307,22 +307,63 @@ arma::velocity::High_amplitude_realtime_solver<T>::compute_second_function(
 	const int nz = this->_domain.num_points(1);
 	const int nx = zeta.extent(1);
 	const int ny = zeta.extent(2);
+    using opencl::context;
+    typedef opencl::Vec<Vector<T,3>,T,3> Vec3;
+    const Vector<size_t,3> shp(zeta.shape());
+    cl::Buffer bzeta(
+            context(),
+            const_cast<T*>(zeta.data()),
+            const_cast<T*>(zeta.data() + zeta.numElements()),
+            true
+    );
+	size_t max_memory = opencl::devices()[0].getInfo<CL_DEVICE_LOCAL_MEM_SIZE>();
+    cl::Kernel kernel = opencl::get_kernel(__func__);
+    kernel.setArg(0, bzeta);
+    kernel.setArg(1, Vec3(zeta.grid().length()));
+    kernel.setArg(2, 0); // t derivative
+    kernel.setArg(3, this->_phi);
+	kernel.setArg(4, max_memory, nullptr);
+    opencl::command_queue().enqueueNDRangeKernel(
+            kernel,
+            cl::NullRange,
+            cl::NDRange(shp(0), shp(1), shp(2))
+    );
+	/*
+	
+	Array3D<T> zeta_t(zeta.shape());
+	for (int i = 0; i < nt; ++i)
+	{
+		using blitz::Range;
+		zeta_t(i, Range::all(), Range::all()) = -derivative<0, T>(
+			zeta,
+			zeta.grid().delta(),
+			i);
+		cl::copy(
+                opencl::command_queue(),
+                zeta_t.data(),
+                zeta_t.data() + zeta_t.numElements(),
+                this->_sfunc
+			);
+        }
+*/
+	/*
+	const int nt = this->_domain.num_points(0);
 	Array3D<std::complex<T>> zeta_t(shape(nz, nx, ny));
-	zeta_t(0, Range::all(), Range::all()) = -derivative<0,T>(
+	zeta_t(0, Range::all(), Range::all()) = -derivative<0, T>(
 		zeta,
 		zeta.grid().delta(),
-		idx_t
-	);
-	for (int i=1; i<nz; ++i) {
+		idx_t);
+	for (int i = 1; i < nz; ++i)
+	{
 		zeta_t(i, Range::all(), Range::all()) = zeta_t(0, Range::all(), Range::all());
-	}
-	cl::copy(
-		opencl::command_queue(),
-		zeta_t.data(),
-		zeta_t.data() + zeta_t.numElements(),
-		this->_phi
-	);
-	#if ARMA_DEBUG_FFT
+		}
+		cl::copy(
+			opencl::command_queue(),
+			zeta_t.data(),
+			zeta_t.data() + zeta_t.numElements(),
+			this->_phi);
+*/
+#if ARMA_DEBUG_FFT
 	std::ofstream("zeta_t_opencl")
 		<< blitz::real(zeta_t(0, Range::all(), Range::all()));
 	#endif
